76fec7faecce81504757317ea0921e0defc0be37
ssion03 <sssiion03@gmail.com>
2024-09-17 00:16:46 +0900

    Update update_blog.py
    Signed-off-by: ssion03 <sssiion03@gmail.com>

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -3,14 +3,11 @@ import git
 import os
 
 # 벨로그 RSS 피드 URL
-# example : rss_url = 'https://api.velog.io/rss/@ssion'
 rss_url = 'https://api.velog.io/rss/@ssion'
 
 # 깃허브 레포지토리 경로
 repo_path = '.'
 
-
-    
 # 'velog-posts' 폴더 경로
 posts_dir = os.path.join(repo_path, 'velog-posts')
 print(f"Current working directory: {os.getcwd()}")
@@ -21,7 +18,6 @@ if not os.path.exists(posts_dir):
     os.makedirs(posts_dir)
     print(f"Created directory: {posts_dir}")
 
-
 # 레포지토리 로드
 repo = git.Repo(repo_path)
 
@@ -30,7 +26,6 @@ feed = feedparser.parse(rss_url)
 print(f"Feed title: {feed.feed.title}")
 print(f"Feed link: {feed.feed.link}")
 print(f"Feed description: {feed.feed.description}")
-print(f"Feed entrice: {feed["entries"]}")
 
 # 각 글을 파일로 저장하고 커밋
 for entry in feed.entries:
@@ -38,7 +33,13 @@ for entry in feed.entries:
     file_name = entry.title
     file_name = file_name.replace('/', '-')  # 슬래시를 대시로 대체
     file_name = file_name.replace('\\', '-')  # 백슬래시를 대시로 대체
-    # 필요에 따라 추가 문자 대체
+    file_name = file_name.replace(':', '-')  # 콜론을 대시로 대체 (윈도우에서 파일명에 사용 안됨)
+    file_name = file_name.replace('*', '-')  # 별표를 대시로 대체
+    file_name = file_name.replace('?', '-')  # 물음표를 대시로 대체
+    file_name = file_name.replace('"', '-')  # 큰따옴표를 대시로 대체
+    file_name = file_name.replace('<', '-')  # <를 대시로 대체
+    file_name = file_name.replace('>', '-')  # >를 대시로 대체
+    file_name = file_name.replace('|', '-')  # |를 대시로 대체
     file_name += '.md'
     file_path = os.path.join(posts_dir, file_name)
     print(f"File path: {file_path}")
@@ -57,14 +58,19 @@ for entry in feed.entries:
             with open(file_path, 'w', encoding='utf-8') as file:
                 file.write(new_content)
             print(f"Updated file: {file_path}")
-        # 깃허브 커밋
-        try:
-            repo.git.add(file_path)
-            repo.git.commit('-m', f'Add post: {entry.title}')
-            print(f"Committed file: {file_path}")
-        except git.exc.GitCommandError as e:
-            print(f"Git error: {e}")            
-        
+    else:
+        # 파일 새로 생성
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(entry.description)
+        print(f"Created new file: {file_path}")
+
+    # 깃허브 커밋
+    try:
+        repo.git.add(file_path)
+        repo.git.commit('-m', f'Add or update post: {entry.title}')
+        print(f"Committed file: {file_path}")
+    except git.exc.GitCommandError as e:
+        print(f"Git error: {e}")
 
 # 변경 사항을 깃허브에 푸시
 try:
